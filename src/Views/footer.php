8442ae8262a1947fb10168b6775e1aff3f8485ef
LOI-mln <milan.loi@etu.unilim.fr>
2025-09-26 18:24:42 +0200

    pied des pages avec bootstrap

--- src/Views/footer.php
+++ src/Views/footer.php
@@ -1,4 +1,10 @@
-    </div>
-<footer>
-    <div>&copy; 2025 - IUT</div>
-</footer>
+<?php // src/Views/footer.php ?>
+    </main>
+
+    <footer class="bg-dark text-light text-center py-3 mt-auto">
+        <div class="container">
+            &copy; <?= date('Y') ?> - IUT
+        </div>
+    </footer>
+</body>
+</html>
