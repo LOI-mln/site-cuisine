087dd20bea735aed926e196c234059781243be00
LOI-mln <milan.loi@etu.unilim.fr>
2025-09-26 18:11:50 +0200

    ajout page contact

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <?php
-// connexion à la base de données
-require_once(__DIR__.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'Models'.DIRECTORY_SEPARATOR.'connectDb.php');
-// ajout de l'en tête
-require_once(__DIR__.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'Views'.DIRECTORY_SEPARATOR.'header.php');
-// inclusion du corps de la page
-require_once(__DIR__.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'Controllers'.DIRECTORY_SEPARATOR.'homeController.php');
-// ajout du pied de page
-require_once(__DIR__.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'Views'.DIRECTORY_SEPARATOR.'footer.php');
+
+require_once __DIR__.'/src/Models/connectDb.php';
+
+
+require_once __DIR__.'/src/Views/header.php';
+
+$c = $_GET['c'] ?? 'home';
+
+switch ($c) {
+    case 'contact':
+        require_once __DIR__.'/src/Controllers/contactController.php';
+        break;
+
+    case 'home':
+    default:
+        require_once __DIR__.'/src/Controllers/homeController.php';
+        break;
+}
+
+
+require_once __DIR__.'/src/Views/footer.php';
--- src/Controllers/homeController.php
+++ src/Controllers/homeController.php
@@ -1,4 +1,2 @@
 <?php
-
-    require_once
-__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'Views'.DIRECTORY_SEPARATOR.'home.php';
+require_once __DIR__.'/../Views/home.php';
