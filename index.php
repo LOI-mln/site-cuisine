73bdd88980df0a201be43057822339c175f4c0d8
LOI-mln <milan.loi@etu.unilim.fr>
2025-10-21 15:45:06 +0200

    lien des controleurs

--- index.php
+++ index.php
@@ -6,8 +6,19 @@ session_start();
 define('BASE_PATH', __DIR__);
 define('SRC_PATH', BASE_PATH . '/src');
 
-// Commun
-require_once SRC_PATH . '/Controllers/recette/RecetteController.php';      // doit définir $pdo
+// --- GESTION PDO ---
+// On inclut Database.php pour créer $pdo
+require_once SRC_PATH . '/Models/Database.php'; 
+// On inclut les contrôleurs
+require_once SRC_PATH . '/Controllers/recette/RecetteController.php';
+require_once SRC_PATH . '/Controllers/contact/ContactController.php';
+require_once SRC_PATH . '/Controllers/user/UserController.php';
+
+// On crée la connexion PDO
+$database = new Database();
+$pdo = $database->getConnection();
+// --- FIN GESTION PDO ---
+
 require_once SRC_PATH . '/Views/forme/header.php';
 
 $route = $_GET['c'] ?? 'home';
@@ -15,47 +26,85 @@ $option = $_GET['a'] ?? 'index';
 
 try {
     switch ($route) {
-
         case 'Recette' :
-
+            $ctrl = new RecetteController();
             switch ($option){
                 case 'ajout':
-                    require_once SRC_PATH . '/Controllers/recette/RecetteController.php';
-                    $ctrl = new RecetteController();
                     $ctrl->ajout();
                     break;
-
                 case 'liste':
-                    require_once SRC_PATH . '/Controllers/recette/RecetteController.php';
-                    $ctrl = new RecetteController();
                     $ctrl->liste();
                     break;
-
                 case 'enregistrer':
-                    require_once SRC_PATH . '/Controllers/recette/RecetteController.php';
-                   
-                    $ctrl = new RecetteController();
-                    $ctrl->enregistrer($pdo);
+                    $ctrl->enregistrer();
+                    break;
+                case 'detail':
+                    $id = $_GET['id'] ?? null;
+                    if ($id === null) {
+                        throw new Exception("ID de recette manquant.");
+                    }
+                    $ctrl->detail((int)$id);
+                    break;
+                
+                // MODIFICATION ICI : AJOUT DE LA ROUTE 'modifier' [cite: 158]
+                case 'modifier':
+                    $id = $_GET['id'] ?? null;
+                    if ($id === null) {
+                        throw new Exception("ID de recette manquant.");
+                    }
+                    $ctrl->modifier((int)$id); // Appelle la fonction de l'étape 13
+                    break;
+                
+                default:
+                    $ctrl->liste();
                     break;
                 }break;
 
         case 'Contact':
+            $ctrl = new ContactController();
 
             switch($option){
-
                 case 'ajouter':
-                    require_once SRC_PATH . '/Controllers/contact/ContactController.php';
-                    $ctrl = new ContactController();
                     $ctrl->ajouter();
                     break;
-
                 case 'enregistrer':
-                    require_once SRC_PATH . '/Controllers/contact/ContactController.php';
-                    $ctrl = new ContactController();
-                    $ctrl->enregistrer($pdo);
+                    // ContactController a besoin de $pdo
+                    $ctrl->enregistrer($pdo); 
                     break;
                 }break;
-
+        
+        case 'User':
+            $ctrl = new UserController();
+            switch ($option) {
+                case 'inscription':
+                    $ctrl->inscription();
+                    break;
+                case 'inscrire':
+                    $ctrl->inscrire();
+                    break;
+                
+                // AJOUT DES NOUVELLES ROUTES
+                case 'connexion': // Affiche le formulaire
+                    $ctrl->connexion();
+                    break;
+                case 'connecter': // Traite le formulaire
+                    $ctrl->connecter();
+                    break;
+                case 'deconnexion':
+                    $ctrl->deconnexion();
+                    break;
+                case 'profil':
+                    $ctrl->profil();
+                    break;
+                
+                // (On ajoutera 'profil' ici)
+                
+                default:
+                    $ctrl->inscription();
+                    break;
+            }
+            break;
+        
         case 'home':
             default:
                 require_once SRC_PATH . '/Controllers/homeController.php';
